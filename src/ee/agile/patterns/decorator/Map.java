1c9e6c87e568b1f6175bb0b10c6ce287419db662
Anton Keks <anton@codeborne.com>
2011-10-29 00:23:38 +0300

    better world map, map of tallinn removed

--- src/ee/agile/patterns/decorator/Map.java
+++ src/ee/agile/patterns/decorator/Map.java
@@ -2,6 +2,7 @@ package ee.agile.patterns.decorator;
 
 public interface Map {
     char get(int x, int y);
+    String line(int y);
 
     int width();
     int height();
--- src/ee/agile/patterns/decorator/MapVisualizer.java
+++ src/ee/agile/patterns/decorator/MapVisualizer.java
@@ -3,9 +3,20 @@ package ee.agile.patterns.decorator;
 public class MapVisualizer {
     public String toString(Map map) {
         StringBuilder buf = new StringBuilder();
+        appendLine(buf, map.width());
+        for (int y = 0; y < map.height(); y++)
+            buf.append('|').append(map.line(y)).append("|\n");
+        appendLine(buf, map.width());
+        return buf.toString();
+    }
+
+    private StringBuilder appendLine(StringBuilder buf, int length) {
+        buf.append('+');
+        for (int i = 0; i < length; i++) buf.append("-");
+        return buf.append("+\n");
     }
 
     public static void main(String[] args) {
-
+        System.out.println(new MapVisualizer().toString(new WorldMap()));
     }
 }
