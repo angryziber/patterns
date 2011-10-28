c62bd216c546908bb332d7c4d4b5aaec98084604
Anton Keks <anton@codeborne.com>
2011-10-28 22:53:28 +0300

    revelation is now a constant

--- test/ee/agile/patterns/composite/UnzipperTest.java
+++ test/ee/agile/patterns/composite/UnzipperTest.java
@@ -7,17 +7,17 @@ import static junit.framework.Assert.*;
 public class UnzipperTest {
     private Unzipper unzipper = new Unzipper();
 
-    @Test // Bible, Rev.13:18
+    public static final String REVELATION = // Bible, Rev.13:18
+            "Here is wisdom. Let him who has understanding calculate " +
+            "the number of the beast, for it is the number of a man: " +
+            "his number is six hundred and sixty- six.";
+
+    @Test
     public void revelationCompressesWell() throws Exception {
         String zipped = "Here~=%. Let~HW$UC!1O! beast, for~-=!1OaM: his~1=60&6ty-~6.";
         String unzipped = unzipper.unzip(zipped);
 
-        assertEquals(
-            "Here is wisdom. Let him who has understanding calculate " +
-            "the number of the beast, for it is the number of a man: " +
-            "his number is six hundred and sixty- six.",
-            unzipped);
-
+        assertEquals(REVELATION, unzipped);
         assertEquals(59, zipped.length());
         assertEquals(153, unzipped.length());
     }
