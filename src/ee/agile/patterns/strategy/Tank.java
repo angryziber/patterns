86a1c410bf85213d15e77f814d6513e44d1c3912
Anton Keks <anton@codeborne.com>
2011-10-29 17:24:57 +0300

    strategy pattern

--- src/ee/agile/patterns/strategy/Tank.java
+++ src/ee/agile/patterns/strategy/Tank.java
@@ -1,11 +1,28 @@
 package ee.agile.patterns.strategy;
 
+import static ee.agile.patterns.strategy.Tank.Strategy.*;
+
 public class Tank implements Fighter {
+    enum Strategy {
+        RUN_AWAY,
+        RUN_OVER_THE_TROOPER,
+        ONE_TO_ONE_WITH_ANOTHER_TANK,
+        RANDOM_BATTLE;
+
+        public void doSomething() {
+            // every strategy would move and shoot differently
+        }
+    }
+
+    private Strategy currentStrategy = RANDOM_BATTLE;
+
     public void action() {
-        System.out.println("Doing something!");
+        currentStrategy.doSomething();
     }
 
     public void hitBy(Fighter enemy) {
-        System.out.println("Ouch!");
+        if (enemy instanceof Tank) {
+            currentStrategy = ONE_TO_ONE_WITH_ANOTHER_TANK;
+        }
     }
 }
